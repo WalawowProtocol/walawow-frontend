2654ad64703dab40028f1a3264e8396d2dc6e2fb
Walawow <niyoo999@gmail.com>
2025-12-25 15:39:05 +0800

    Update addresses.ts

--- config/addresses.ts
+++ config/addresses.ts
@@ -12,44 +12,48 @@ export const WALAWOW_PROTOCOL_ADDRESSES = {
   // ==================== 程序 ID ====================
   HARVEST_PROGRAM: harvestIdl.address,        // EGgcfctL62Eaq8kG6T9gPjWSjNktx3edRVXWVm4e7R2r
   SWAP_PROGRAM: swapIdl.address,              // 93gyiehkL4mAQbqJViRWj3eq2FtGusy1SzuGLefNjamZ
-  DISTRIBUTOR_PROGRAM: distributorIdl.address,// G9XaeLFJbmjrZUsEoky5YiRG4Fr7j5ZQNernvfrjfX3f
-  POOL_PROGRAM: poolIdl.address,              // FJxHKnBz1MrbAE4UB8LFMMniPcELYjvpwLpRaiEDuDjq
+  DISTRIBUTOR_PROGRAM: "Dr4qqXPEsbGt5TTuTbBcFSeqCUEAGv6ZNbZdwLTX8yTe",
+  POOL_PROGRAM: "DNLgTgV56ifjHpP7V1iodtUoYwrnawq1tYMEwm2yD3KA",
 
   // ==================== Token Mint ====================
-  WALAWOW_MINT: "DPuHouX7yJP484mBPuSxwPn9xKZPzUEue37cPbGXpG8D", // Token-2022 (taxed)
-  USDC_MINT: "ECjFKugJfzXEbLD3TkkewRAjJs38bssNpXGxEfKfKgHT",    // Token-2022 USDC
+  WALAWOW_MINT: "vqvzPQ59Pdg5PUAYXeZ1sX5kiqNpGSXhaauXfE7PpZ2", // Token-2022 (taxed)
+  USDC_MINT: "CBXMnFSp5Uxyybqv4vRik1NyhkixDAt4SYcU1sTZbKRL",    // Token-2022 USDC
 
   // ==================== Pool PDA ====================
-  POOL_WEEKLY: "BVmZXGSPq2z2d6V4n3tMHCi5DkLhZ24gzYRA17zp7EXQ",
-  POOL_MONTHLY: "9KFBHKFqe7wiR9MTfizwGT75DrNb7dqUQVeZA1kKXV17",
+  POOL_WEEKLY: "6ZuQ8Sokhm5ovXmaiMhSG2j5jgcWzYFR1WCUK6fDAmr8",
+  POOL_MONTHLY: "PTz3PS5LWs96stMLRuQgU72HTPjGwhaaVFdHnTxmJcJ",
+  POOL_WEEKLY_AUTHORITY: "GPJpB3eidJbj9NALkAqFXPLhiVNDWK5NsCrBQkW6FoDG",
+  POOL_MONTHLY_AUTHORITY: "9hBWexge1ZLyv9YMJpr2PUnHZr5iSK2zr748tzZz47LD",
 
   // ==================== Distributor PDA ====================
-  DISTRIBUTOR_CONFIG: "Cxf2fzY7M7rUuxVyeea6tYMzeskB6NYNmhHtqawzAgvT",
-  DISTRIBUTOR_AUTHORITY: "2gZMATJi6k9gNaTxWLarmv5ftoxo5dwgn7b3YSsh8a9d",
-  DISTRIBUTOR_VAULT: "2DMad4xrWx5DJn7GVX8AsgAL72rgcP2myqqaDKHYrdBD",
+  DISTRIBUTOR_CONFIG: "FUTmTfzZrzebkDRNmb3jmKY1mycvsDSL4U9WASXNpVWd",
+  DISTRIBUTOR_AUTHORITY: "3H3dbH15zcxDAuQya5WeQSA8rFrYGuSvssjFMGFBHRBW",
+  DISTRIBUTOR_VAULT: "EFSFzKvJCzTeJeq1Hy8w2SxqdWHk4dAmFLn8BS44EyBN",
 
   // Distributor 分配池（USDC vaults）
-  DISTRIBUTOR_POOL_WEEKLY_VAULT: "8qvntR8JsWMFB29ya9zN7GhpwLDpMtWe5eQoo3nHmHWG",
-  DISTRIBUTOR_POOL_MONTHLY_VAULT: "3rjPZTvFNCFiHNxLjtF9EqqHpD8KAZzaoYXyzFYf9Bpz",
-  DISTRIBUTOR_POOL_STAKING_VAULT: "Fm5z6iNzjuCdozMf6MbD337egkcQKg7y9ucYub26spuA",
-  DISTRIBUTOR_POOL_LIQUIDITY_VAULT: "93cUizoZA1sCBhL9cJb33dD4z1JzBvYx5j68speQmEhh",
-  DISTRIBUTOR_POOL_TEAM_VAULT: "2TXdjfZp9C2yd6oggdC3WmKVaqRadpJopS99nbXLhAxB",
+  DISTRIBUTOR_POOL_WEEKLY_VAULT: "8VzFpNAewszz3947vefjBUpxsX1DMr9s5XCCcxNqpuqc",
+  DISTRIBUTOR_POOL_MONTHLY_VAULT: "5T8UXA2pZvJ2GTNGo6cs3cBxx8xXgC5JwNz8C4tsfigN",
+  DISTRIBUTOR_POOL_STAKING_VAULT: "5mLjBNFnsUB27VPyquBDioBpMG6BMarTnnuK8UDFGfQn",
+  DISTRIBUTOR_POOL_LIQUIDITY_VAULT: "2qQv7vrh2j7LveL44iZfc5PgVoCr6WNgq7XhGpj1vRHj",
+  DISTRIBUTOR_POOL_TEAM_VAULT: "4atbKZz5DufzxSwzkEjY7cdd2waZ67stfyBLNo34M6JG",
+  WEEKLY_STAGING_VAULT: "HGCkYZEnpcGzCEQ8FukBJRVnYYPzrhoTp1TvgViejEC5",
+  MONTHLY_STAGING_VAULT: "GhURdu6UCP8pMv4vdbS1tApWn7LsY49GFp83r2XvJAiU",
 
   // ==================== Harvest PDA ====================
-  HARVEST_CONFIG: "5YBroUkpWqFyRLiRDpZ5FB3yrmb7RmUE12677yDU8cYG",
-  HARVEST_AUTHORITY: "GYraorLj8MkAYZCmuXoQ15Fgd4cdm4enV5X2edHpWnKa",
+  HARVEST_CONFIG: "5roJrf7tNxTpcUdsdbD96hTZkdufACJq98GE1zMNKavV",
+  HARVEST_AUTHORITY: "DimqWk8eRXxo7YEUV3TGENyZAcKZcmdc9ZX211o4kdDG",
 
   // ==================== Swap PDA ====================
-  SWAP_CONFIG: "6xdVadzBTnMWNTPEiagdFsLQrEngMYYrjxKu4BcgEcZJ",
-  SWAP_AUTHORITY: "6vKksMUCvdvkF8dFrWYJpkhjJSSAJLuNXkDjCsHrj5xB",
-  SWAP_TAX_VAULT: "DqiFpRhw3mSf9e7ZGVWS64giHnmWsgzGW5awwypXLdCP", // WALAWOW tax
-  SWAP_USDC_VAULT: "24WFYqUAcVbm4pMHBUJp6Ch56ewJ7dp2eq2wqDsEUfjS",
+  SWAP_CONFIG: "MQbvsddgxMQ3io45DrzDeptXPQokf5tXYJBactuDoUB",
+  SWAP_AUTHORITY: "EojxfsA32ks8utWM4PJmB2eJdDr653tBk1gQ1dbS9hgX",
+  SWAP_TAX_VAULT: "4rPtUXXN7FUtmWeNUBqBU428Vs1ueZHRwdfeQaVqtQJd", // WALAWOW tax
+  SWAP_USDC_VAULT: "9tiqFmNxGmtdWcwn4so6ChJKx6BBrWMVUBwUuk9Yu7Lw",
 
   // ==================== VRF / Keeper ====================
   VRF_COORDINATOR: "HifRke4NrpDcget6FRwW2aryZDWgdiURVh8ewduaoepx",
 
   // ==================== RPC ====================
-  RPC_URL: "https://devnet.helius-rpc.com/?api-key=ea4c9b26-b295-4bcb-bba4-9a1ab1007184",
+  RPC_URL: "RL=https://api.devnet.solana.c",
 } as const;
 
 // 向后兼容
